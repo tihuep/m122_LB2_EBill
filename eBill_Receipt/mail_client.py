d33bd6173f1fa4a0bc3d5fd9a869fbb4ece2458e
Timon <timon.hueppi@gmail.com>
2021-06-29 20:51:42 +0200

    adds email handling

---
+++ eBill_Receipt/mail_client.py
@@ -0,0 +1,53 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from pathlib import Path
+
+def send_email(email, filename, invoice_nr):
+    sender_mail = "ebill.hueppi.tbz@gmail.com"
+    password = "Berufsschule8005!"
+
+    message = MIMEMultipart()
+    message['From'] = sender_mail
+    message['To'] = email
+    message['Subject'] = f'Erfolgte Verarbeitung Rechnung {invoice_nr}'
+
+    firstname = 'Max'
+    lastname = 'Mustermann'
+    date = '69.69.6969'
+
+    message.attach(MIMEText(f'''Sehr geehrte(r) {firstname} {lastname},
+
+Am {date} wurde die erfolgreiche Bearbeitung der Rechnung {invoice_nr}
+vom Zahlungssystem <<>> gemeldet.
+
+Mit freundlichen Grüssen
+
+Timon Hüppi
+TBZ - Technische Berufsschule Zürich
+''', 'plain'))
+    attach_file = open(filename, 'rb')
+    payload = MIMEBase('application', 'octate-stream')
+    payload.set_payload(attach_file.read())
+    encoders.encode_base64(payload)
+
+    payload.add_header('Content-Decomposition', 'attachment', filename=filename)
+    message.attach(payload)
+
+    session = get_gmail_smtp_connection(sender_mail, password)
+
+    text = message.as_string()
+    result = session.sendmail(sender_mail, email, text)
+    session.quit()
+
+    return result
+
+def get_gmail_smtp_connection(mail, password):
+    port = 465
+    smtp_server_domain_name = "smtp.gmail.com"
+    ssl_context = ssl.create_default_context()
+    service = smtplib.SMTP_SSL(smtp_server_domain_name, port, context=ssl_context)
+    service.login(mail, password)
+    return service
--- eBill_Receipt/main.py
+++ eBill_Receipt/main.py
@@ -2,6 +2,7 @@ import banking_system_client
 import receipt_handler
 import client_system_client
 import mail_client
+import os
 
 receipts = banking_system_client.download_receipts()
 if (receipts == 0):
@@ -11,6 +12,13 @@ invoices = receipt_handler.get_all_invoices_from_receipts()
 
 for invoice in invoices:
     zip_filename = client_system_client.make_zip(invoice[2], invoice[1], invoice[0])
-    print(zip_filename)
     client_system_client.upload_receipt_zip('../tmp/zips/' + invoice[0] + '.zip')
-    #send
+    mail_client.send_email(invoice[3], zip_filename, invoice[0])
+    if os.path.exists(zip_filename):
+        os.remove(zip_filename)
+    if os.path.exists('../tmp/six_files/' + invoice[2]):
+        os.remove('../tmp/six_files/' + invoice[2])
+
+for invoice in invoices:
+    if os.path.exists('../tmp/receipts/' + invoice[1]):
+        os.remove('../tmp/receipts/' + invoice[1])
